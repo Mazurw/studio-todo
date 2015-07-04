4c5cfef7ce7b1712c73f5707f5c9023d97dbd48b
Mazurw <noreply>
2015-07-04 12:35:51 +0000

    Add component Baner

---
+++ ui/baner.reel/baner.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/baner.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Baner
+ * @extends Component
+ */
+exports.Baner = Component.specialize(/** @lends Baner# */ {
+    constructor: {
+        value: function Baner() {
+            this.super();
+        }
+    }
+});
